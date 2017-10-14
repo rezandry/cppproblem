164aa0368c549d750080c8211f71c8e5de8d4b9e
rezandry <andriyunantoreza@gmail.com>
2017-10-14 20:47:22 +0700

    adding problem tablex

--- encrypt.cpp
+++ encrypt.cpp
@@ -4,13 +4,22 @@ using namespace std;
 
 main ()
 {
+    // algo :
+    // 1. masukan input 
+    // 2. jika ascii input <65 dan >90 (ASCII A-Z terletak di 65-90)
+    // 3. maka print biasa
+    // 4. selain itu, jika ASCII input+10>90 (10 abjad terakhir)
+    // 5. maka ASCII dikurangi 16 (16 didapat dari -26+10, abjad Z di ASCII adalah 90, maka 90-26+10 = 90-16)
+    // 6. tambahan, maka abjad terakhir dimulai dari Q, yang mana asciinya adalah 81, maka akan menjadi A dengan hasil 81-16 = 65 (A)
+    // 7. selain itu maka dicetak dengan penambahan nilai ASCII +10
+
   string input;
   int i;
   getline(cin, input);
   for (i=0;i < input.size();i++){
     if (input[i] < 65 || input[i] >90){
         cout << char(input[i]);
-    }else if (input[i]+10 > 90){
+    }else if (input[i] > 80){
         cout << char(input[i]-16);    
     }else{
         cout << char(input[i]+10);
--- tablex.cpp
+++ tablex.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 main ()
 {
+    // Algo :
+    // 1. masukkan input 
+    // 2. untuk setiap bari ke i, dan kolom j, 
+    // 3. cetak i*j 
+
   int input;
   cin >> input;
   for (int i=1;i<=input;i++){
